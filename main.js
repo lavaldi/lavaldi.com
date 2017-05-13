513c7700d6d3b338c64467c01975dd10c1e9627a
Claudia Valdivieso <claumavaldivieso@gmail.com>
2017-05-13 17:09:10 -0500

    create the web! 🙌

---
+++ main.js
@@ -0,0 +1,13 @@
+(function () {
+	'use strict';
+    window.handleRssPost = function (data) {
+		var item = data.query.results.rss.channel.item;
+		var link = document.createElement('a');
+		link.href = item.link.replace(/\?source=rss.*$/, '');
+		link.target = "_blank";
+		link.textContent = item.title;
+		var container = document.querySelector('#latest-blog-post');
+		container.appendChild(link);
+		container.classList.add('visible');
+	};
+})();
