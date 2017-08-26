9d06595f7bc27b1c1987196b9d764f2a6712b257
Claudia Valdivieso <claumavaldivieso@gmail.com>
2017-08-26 18:09:07 -0500

    feed validation

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 (function () {
 	'use strict';
     window.handleRssPost = function (data) {
-		var item = data.query.results.rss.channel.item;
-		var link = document.createElement('a');
-		link.href = item.link.replace(/\?source=rss.*$/, '');
-		link.target = "_blank";
-		link.textContent = item.title;
-		var container = document.querySelector('#latest-blog-post');
-		container.appendChild(link);
-		container.classList.add('visible');
+		if(data.query.results !== null) {
+			var item = data.query.results.rss.channel.item;
+			var link = document.createElement('a');
+			link.href = item.link.replace(/\?source=rss.*$/, '');
+			link.target = "_blank";
+			link.textContent = item.title;
+			var container = document.querySelector('#latest-blog-post');
+			container.appendChild(link);
+			container.classList.add('visible');
+		}
+		
 	};
 })();
