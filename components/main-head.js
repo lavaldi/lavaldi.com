bdb970c313ce985e1bf9e7f6a7ae9e9e5bba807b
Claudia Valdivieso <claumavaldivieso@gmail.com>
2018-02-25 19:43:42 -0500

    change the title of the web

--- components/main-head.js
+++ components/main-head.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
 export default ({ data }) => {
-  const image = (data.image) ? data.image : "http://lavaldi.com/static/lavaldi.jpg"
+  const image = data.image
+    ? data.image
+    : "http://lavaldi.com/static/lavaldi.jpg";
   return (
     <Head>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+      />
       <meta name="autor" content="Claudia Valdivieso" />
       <meta name="description" content={data.description} />
-      <meta property="og:title" content={"La Valdi | " + data.title} />
+      <meta property="og:title" content={data.title + " - La Valdi"} />
       <meta property="og:type" content="website" />
       {/*<meta property="og:url" content="https://lavaldi.com" />*/}
       <meta property="og:image" content={image} />
@@ -20,9 +25,9 @@ export default ({ data }) => {
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@lavaldi_" />
       <meta name="twitter:creator" content="@lavaldi_" />
-      <title>La Valdi | {data.title}</title>
+      <title>{data.title} - La Valdi</title>
       <link rel="icon" href="/static/favicon.png" sizes="32x32" />
       <link rel="stylesheet" href="/static/main.css" />
     </Head>
-  )
-}
+  );
+};
