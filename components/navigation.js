be87dbfeed2c9c747f0efd41b947e9beb1e19122
Claudia Valdivieso <claumavaldivieso@gmail.com>
2018-09-15 19:08:27 -0500

    add Apps tab for Verbs PWA

--- components/navigation.js
+++ components/navigation.js
@@ -3,14 +3,14 @@ import Link from 'nextein/link'
 
 class Navigation extends Component {
 
-  toggleMenu () {
+  toggleMenu() {
     const burger = document.querySelector('.navbar-burger');
     const menu = document.querySelector('.navbar-menu');
     burger.classList.toggle('is-active');
     menu.classList.toggle('is-active');
   };
 
-  render () {
+  render() {
     return (
       <div className="hero-head">
         <div className="container">
@@ -18,24 +18,31 @@ class Navigation extends Component {
             <div className="navbar-brand">
               <Link href="/">
                 <a className="navbar-item">
-                  <img src={'/static/lavaldi-icon.png'} alt="La Valdi" width="25" height="25"/>
+                  <img src={'/static/lavaldi-icon.png'} alt="La Valdi" width="25" height="25" />
                 </a>
               </Link>
-        
+
               <div className="navbar-burger burger" onClick={this.toggleMenu}>
                 <span></span>
                 <span></span>
                 <span></span>
               </div>
             </div>
-        
+
             <div id="navMenuTransparentExample" className="navbar-menu">
               <div className="navbar-end">
-                {/*<Link href="/writings">
-                  <a className="nav-item is-tab">
-                    Writings
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">
+                    Apps
                   </a>
-                </Link>*/}
+                  <div className="navbar-dropdown is-right">
+                    <Link href="https://verbs.lavaldi.com">
+                      <a className="navbar-item">
+                        <span style={styles.emoji}>🇬🇧</span> Verbs
+                      </a>
+                    </Link>
+                  </div>
+                </div>
                 <Link href="/jesusfreak">
                   <a className="nav-item is-tab">
                     <span style={styles.emoji}>🙌</span> Jesus Freak
@@ -43,7 +50,7 @@ class Navigation extends Component {
                 </Link>
                 <Link href="/code">
                   <a className="nav-item is-tab">
-                  <span style={styles.emoji}>💻</span> Front End
+                    <span style={styles.emoji}>💻</span> Front End
                   </a>
                 </Link>
                 <span className="nav-item">
@@ -53,7 +60,7 @@ class Navigation extends Component {
             </div>
           </nav>
         </div>
-      </div>
+      </div >
     )
   }
 }
@@ -67,40 +74,40 @@ const styles = {
   }
 };
 
-/*
+    /*
 <div className="hero-head">
-      <div className="container">
-        <nav className="navbar has-shadow">
           <div className="container">
-            <div className="nav-left">
-              <Link href="/">
-                <a className="nav-item" title="La Valdi">
-                  <img src={'../static/lavaldi-icon.png'} alt="La Valdi" />
+            <nav className="navbar has-shadow">
+              <div className="container">
+                <div className="nav-left">
+                  <Link href="/">
+                    <a className="nav-item" title="La Valdi">
+                      <img src={'../static/lavaldi-icon.png'} alt="La Valdi" />
+                    </a>
+                  </Link>
+                </div>
+                <span className="nav-toggle">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+                <div className="nav-right nav-menu">
+                  <Link href="/jesus-freak">
+                    <a className="nav-item is-tab">
+                      <span className="bd-emoji">🙌</span> Jesus Freak
                 </a>
-              </Link>
-            </div>
-            <span className="nav-toggle">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-            <div className="nav-right nav-menu">
-              <Link href="/jesus-freak">
-                <a className="nav-item is-tab">
-                  <span className="bd-emoji">🙌</span> Jesus Freak
+                  </Link>
+                  <Link href="/code">
+                    <a className="nav-item is-tab">
+                      <span className="bd-emoji">💻</span> Code
                 </a>
-              </Link>
-              <Link href="/code">
-                <a className="nav-item is-tab">
-                <span className="bd-emoji">💻</span> Code
-                </a>
-              </Link>
-              <span className="nav-item">
-                <a className="button" href="mailto:claumavaldivieso@gmail.com">Contact me</a>
-              </span>
-            </div>
+                  </Link>
+                  <span className="nav-item">
+                    <a className="button" href="mailto:claumavaldivieso@gmail.com">Contact me</a>
+                  </span>
+                </div>
+              </div>
+            </nav>
           </div>
-        </nav>
-      </div>
-    </div>
-    */
+        </div>
+        */
