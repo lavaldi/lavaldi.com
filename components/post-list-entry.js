b719115f3856fdcf7c3aee1724ff176ebb4da5c3
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-01-03 20:16:35 -0500

    simplify article listing

--- components/post-list-entry.js
+++ components/post-list-entry.js
@@ -2,47 +2,31 @@
 import React from 'react'
 import Moment from 'react-moment'
 import Link from 'nextein/link'
-import { Content } from 'nextein/post'
 
-import Tags from './tags'
-
-export default ({ data, content, excerpt = true }) => {
-  const { url, tag, title, date, _entry, page = 'post' } = data
+export default ({ data, content }) => {
+  const { title, date = 'post' } = data
 
   return (
-    <article className="content is-medium" style={styles.article}>
-      <div className="content-header">
-        <h1 className="title">
-          <Link data={data} content={content}><a>{title}</a></Link>
-        </h1>
-        <span className="tags">
-          <Moment locale="es" format="LL" add={{ hours: 5 }} style={styles.moment}>{date}</Moment>
-          <Tags tags={tag} />
-        </span>
-      </div>
-      <div className="content-body">
-        <Content data={data} content={content} excerpt={excerpt} />
-      </div>
+    <article style={styles.article}>
+      <Moment locale="es" format="LL" add={{ hours: 5 }} style={styles.moment}>{date}</Moment>
+      <Link data={data} content={content}><a>{title}</a></Link>
     </article>
   )
 }
 
 const styles = {
   moment: {
-    alignItems: 'center',
-    borderRadius: '3px',
     color: '#4a4a4a',
-    display: 'inline-flex',
-    fontSize: '.75rem',
+    display: 'inline-block',
+    fontSize: '1rem',
     fontStyle: "italic",
-    height: '2em',
-    justifyContent: 'center',
-    lineHeight: '1.5',
-    whiteSpace: 'nowrap',
-    marginRight: '0.5rem'
+    marginRight: '1rem',
+    textAlign: 'right',
+    width: '195px',
   },
   article: {
-    padding: '1em 1em 3em',
-    borderBottom: '1px solid #f2f2f2'
+    borderBottom: '1px solid #f2f2f2',
+    fontStyle: '1.25rem',
+    padding: '1em',
   }
 };
