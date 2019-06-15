13b6755d75cf5340fe1c16e28ba99d8c26e59ce0
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-06-15 18:43:35 -0500

    rename page from "Front End" to "Code"

--- pages/code.js
+++ pages/code.js
@@ -31,7 +31,7 @@ const Index = props => {
       <section className="hero is-dark">
         <div className="hero-body">
           <div className="container has-text-centered">
-            <h1 className="title">Front End</h1>
+            <h1 className="title">Code</h1>
           </div>
         </div>
       </section>
