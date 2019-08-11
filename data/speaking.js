399d5036f78f1c4cbf8b2a5d005369a64a6f7169
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-08-11 13:53:52 -0500

    add new talks

--- data/speaking.js
+++ data/speaking.js
@@ -1,12 +1,19 @@
 import writing from '../content/thumbnails/writing.png'
 import react from '../content/thumbnails/react.png'
 import graphql from '../content/thumbnails/graphql.png'
+import dom from '../content/thumbnails/dom.png'
+import css from '../content/thumbnails/css.png'
 
 export default [
   {
     title: 'Introducción a GraphQL',
     path: '/graphql-talk',
     img: graphql,
+  },
+  {
+    title: 'Front End Adventures',
+    path: 'https://und-tech-examples.github.io/frontend-adventures/#0',
+    img: dom,
   },
   {
     title: 'Introducción a Toyota Kata',
@@ -17,5 +24,15 @@ export default [
     title: 'SEO & React',
     path: '/seo-react-talk/',
     img: react,
+  },
+  {
+    title: 'Lo chévere de ser Front End',
+    path: 'https://slides.com/lavaldi/lo-chevere-de-ser-frontend',
+    img: dom,
+  },
+  {
+    title: 'Galen Framework',
+    path: 'https://slides.com/lavaldi/galenframework#/',
+    img: css,
   },
 ]
