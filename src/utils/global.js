93deaffdb71ba259d4da47d5e269795ca136a60e
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-08-11 22:36:47 -0500

    change url for "Edit on Github"

--- src/utils/global.js
+++ src/utils/global.js
@@ -6,7 +6,7 @@ const formatDate = date => moment.utc(date).format(config.dateFormat)
 
 const editOnGithub = post => {
   const date = moment.utc(post.date).format(config.dateFromFormat)
-  return urljoin(config.repo, '/blob/master/content/posts', `${date}-${post.slug}.md`)
+  return urljoin(config.repo, '/blob/gatsby/content/posts', `${date}-${post.slug}.md`)
 }
 
 export { formatDate, editOnGithub }
