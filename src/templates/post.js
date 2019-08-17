3a90285551f951473b803b3758aacc42f30c743f
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-08-17 17:07:02 -0500

    open share on twitter in new tab

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import Layout from '../layout'
-import UserInfo from '../components/UserInfo'
-import PostTags from '../components/PostTags'
-import SEO from '../components/SEO'
-import config from '../../data/SiteConfig'
-import { formatDate, editOnGithub } from '../utils/global'
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import Layout from "../layout";
+import UserInfo from "../components/UserInfo";
+import PostTags from "../components/PostTags";
+import SEO from "../components/SEO";
+import config from "../../data/SiteConfig";
+import { formatDate, editOnGithub } from "../utils/global";
 import Disqus from "../components/Disqus";
 
 export default class PostTemplate extends Component {
-
   render() {
-    const { slug } = this.props.pageContext
-    const postNode = this.props.data.markdownRemark
-    const post = postNode.frontmatter
-    let thumbnail
+    const { slug } = this.props.pageContext;
+    const postNode = this.props.data.markdownRemark;
+    const post = postNode.frontmatter;
+    let thumbnail;
 
     if (!post.id) {
-      post.id = slug
+      post.id = slug;
     }
 
     if (!post.category_id) {
-      post.category_id = config.postDefaultCategoryID
+      post.category_id = config.postDefaultCategoryID;
     }
 
     if (post.thumbnail) {
-      thumbnail = post.thumbnail.childImageSharp.fixed
+      thumbnail = post.thumbnail.childImageSharp.fixed;
     }
 
-    const date = formatDate(post.date)
-    const githubLink = editOnGithub(post)
-    const twitterShare = `http://twitter.com/share?text=${encodeURIComponent(post.title)}&url=${
-      config.siteUrl
-      }/${post.slug}/&via=lavaldi_`
+    const date = formatDate(post.date);
+    const githubLink = editOnGithub(post);
+    const twitterShare = `http://twitter.com/share?text=${encodeURIComponent(
+      post.title
+    )}&url=${config.siteUrl}/${post.slug}/&via=lavaldi_`;
 
     return (
       <Layout>
@@ -43,13 +42,22 @@ export default class PostTemplate extends Component {
         </Helmet>
         <SEO postPath={slug} postNode={postNode} postSEO />
         <article className="single container">
-          <header className={`single-header ${!thumbnail ? 'no-thumbnail' : ''}`}>
-            {thumbnail ? <Img fixed={post.thumbnail.childImageSharp.fixed} /> : null}
+          <header
+            className={`single-header ${!thumbnail ? "no-thumbnail" : ""}`}
+          >
+            {thumbnail ? (
+              <Img fixed={post.thumbnail.childImageSharp.fixed} />
+            ) : null}
             <div className="flex">
               <h1>{post.title}</h1>
               <div className="post-meta">
                 <time className="date">{date}</time>/
-                <a className="twitter-link" href={twitterShare}>
+                <a
+                  className="twitter-link"
+                  href={twitterShare}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Share
                 </a>
                 /
@@ -72,7 +80,10 @@ export default class PostTemplate extends Component {
             </div>
           </header>
 
-          <div className="post" dangerouslySetInnerHTML={{ __html: postNode.html }} />
+          <div
+            className="post"
+            dangerouslySetInnerHTML={{ __html: postNode.html }}
+          />
         </article>
 
         <UserInfo config={config} />
@@ -81,7 +92,7 @@ export default class PostTemplate extends Component {
           <Disqus postNode={postNode} />
         </div>
       </Layout>
-    )
+    );
   }
 }
 
@@ -113,4 +124,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
