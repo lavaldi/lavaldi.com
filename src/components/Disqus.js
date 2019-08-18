8de5c4938e09a85c982d6d9fa09f0558a2fc53df
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-08-17 20:59:44 -0500

    fix disqus comments

--- src/components/Disqus.js
+++ src/components/Disqus.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import ReactDisqusComments from "react-disqus-comments";
+import { Disqus as DisqusComments } from "gatsby-plugin-disqus";
 import urljoin from "url-join";
 import config from "../../data/SiteConfig";
 
@@ -11,35 +11,25 @@ class Disqus extends Component {
   onSnackbarDismiss = () => {
     const [, ...toasts] = this.state.toasts;
     this.setState({ toasts });
-  }
+  };
 
   notifyAboutComment = () => {
     const toasts = this.state.toasts.slice();
     toasts.push({ text: "New comment available!" });
     this.setState({ toasts });
-  }
+  };
 
   render() {
     const { postNode } = this.props;
-    if (!config.disqusShortname) {
-      return null;
-    }
     const post = postNode.frontmatter;
-    const url = urljoin(
-      config.siteUrl,
-      post.slug
-    );
+    const url = urljoin(config.siteUrl, post.slug);
+    const disqusConfig = {
+      url,
+      identifier: post.id,
+      title: post.title
+    };
 
-    return (
-      <ReactDisqusComments
-        shortname={config.disqusShortname}
-        identifier={post.id}
-        title={post.title}
-        url={url}
-        category_id={post.category_id}
-        onNewComment={this.notifyAboutComment}
-      />
-    );
+    return <DisqusComments config={disqusConfig} />;
   }
 }
 
