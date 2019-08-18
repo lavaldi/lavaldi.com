b1d21e01b6636610168567e2c379e51e976ee64f
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-08-17 20:59:44 -0500

    fix disqus comments

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,43 +1,43 @@
 const config = {
-  siteTitle: 'Claudia Valdivieso',
-  siteTitleShort: 'Claudia Valdivieso',
-  siteTitleAlt: 'Claudia Valdivieso',
-  siteLogo: '/logos/lavaldi.jpg',
-  siteUrl: 'https://www.lavaldi.com',
-  repo: 'https://github.com/lavaldi/lavaldi.github.com',
-  dateFromFormat: 'YYYY-MM-DD',
-  dateFormat: 'MMMM Do, YYYY',
-  siteDescription:
-    'Claudia Valdivieso, christian, wife and front end.',
-  siteRss: '/rss.xml',
-  googleAnalyticsID: 'UA-99146766-1',
-  disqusShortname: "lavaldi",
-  postDefaultCategoryID: 'code',
-  userName: 'Claudia',
-  userEmail: 'claumavaldivieso@gmail.com',
-  userTwitter: 'lavaldi_',
+  siteTitle: "Claudia Valdivieso",
+  siteTitleShort: "Claudia Valdivieso",
+  siteTitleAlt: "Claudia Valdivieso",
+  siteLogo: "/logos/lavaldi.jpg",
+  siteUrl: "https://www.lavaldi.com",
+  repo: "https://github.com/lavaldi/lavaldi.github.com",
+  dateFromFormat: "YYYY-MM-DD",
+  dateFormat: "MMMM Do, YYYY",
+  siteDescription: "Claudia Valdivieso, christian, wife and front end.",
+  siteRss: "/rss.xml",
+  googleAnalyticsID: "UA-99146766-1",
+  postDefaultCategoryID: "code",
+  userName: "Claudia",
+  userEmail: "claumavaldivieso@gmail.com",
+  userTwitter: "lavaldi_",
   menuLinks: [
     {
-      name: 'Me',
-      link: '/me/',
+      name: "Me",
+      link: "/me/"
     },
     {
-      name: 'Articles',
-      link: '/blog/',
+      name: "Articles",
+      link: "/blog/"
     },
     {
-      name: 'Contact',
-      link: '/contact/',
-    },
+      name: "Contact",
+      link: "/contact/"
+    }
   ],
-  themeColor: '#EB374B', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff',
-}
+  themeColor: "#EB374B", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff"
+};
 
 // Make sure siteUrl doesn't have an ending forward slash
-if (config.siteUrl.substr(-1) === '/') config.siteUrl = config.siteUrl.slice(0, -1)
+if (config.siteUrl.substr(-1) === "/")
+  config.siteUrl = config.siteUrl.slice(0, -1);
 
 // Make sure siteRss has a starting forward slash
-if (config.siteRss && config.siteRss[0] !== '/') config.siteRss = `/${config.siteRss}`
+if (config.siteRss && config.siteRss[0] !== "/")
+  config.siteRss = `/${config.siteRss}`;
 
-module.exports = config
+module.exports = config;
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
-const urljoin = require('url-join')
-const config = require('./data/SiteConfig')
+const urljoin = require("url-join");
+const config = require("./data/SiteConfig");
 
 module.exports = {
   siteMetadata: {
@@ -9,114 +9,114 @@ module.exports = {
       feed_url: urljoin(config.siteUrl, config.siteRss),
       title: config.siteTitle,
       description: config.siteDescription,
-      image_url: `${config.siteUrl}/logos/logo-48.png`,
-    },
+      image_url: `${config.siteUrl}/logos/logo-48.png`
+    }
   },
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-twitter',
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-twitter",
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
         headers: {
-          '/*.js': ['cache-control: public, max-age=31536000, immutable'],
-          '/*.css': ['cache-control: public, max-age=31536000, immutable'],
-          '/sw.js': ['cache-control: public, max-age=0, must-revalidate'],
-        },
-      },
+          "/*.js": ["cache-control: public, max-age=31536000, immutable"],
+          "/*.css": ["cache-control: public, max-age=31536000, immutable"],
+          "/sw.js": ["cache-control: public, max-age=0, must-revalidate"]
+        }
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'assets',
-        path: `${__dirname}/static/`,
-      },
+        name: "assets",
+        path: `${__dirname}/static/`
+      }
     },
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: `${__dirname}/src/utils/typography.js`,
-      },
+        pathToConfigModule: `${__dirname}/src/utils/typography.js`
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'posts',
-        path: `${__dirname}/content/`,
-      },
+        name: "posts",
+        path: `${__dirname}/content/`
+      }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
-              maxWidth: 850,
-            },
+              maxWidth: 850
+            }
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
           {
             resolve: `gatsby-remark-autolink-headers`,
             options: {
               offsetY: `100`,
               maintainCase: false,
-              removeAccents: true,
-            },
-          },
-        ],
-      },
+              removeAccents: true
+            }
+          }
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: config.googleAnalyticsID,
-      },
+        trackingId: config.googleAnalyticsID
+      }
     },
     {
-      resolve: 'gatsby-plugin-nprogress',
+      resolve: "gatsby-plugin-nprogress",
       options: {
-        color: config.themeColor,
-      },
+        color: config.themeColor
+      }
     },
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-sharp",
     `gatsby-transformer-sharp`,
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: config.siteTitle,
         short_name: config.siteTitleShort,
         description: config.siteDescription,
-        start_url: '/',
+        start_url: "/",
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: 'minimal-ui',
+        display: "minimal-ui",
         icons: [
           {
-            src: '/logos/logo-432.png',
-            sizes: '32x32',
-            type: 'image/png',
+            src: "/logos/logo-432.png",
+            sizes: "32x32",
+            type: "image/png"
           },
           {
-            src: '/logos/logo-180.png',
-            sizes: '180x180',
-            type: 'image/png',
-          },
-        ],
-      },
+            src: "/logos/logo-180.png",
+            sizes: "180x180",
+            type: "image/png"
+          }
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         setup(ref) {
-          const ret = ref.query.site.siteMetadata.rssMetadata
-          ret.allMarkdownRemark = ref.query.allMarkdownRemark
-          ret.generator = 'Claudia Valdivieso'
-          return ret
+          const ret = ref.query.site.siteMetadata.rssMetadata;
+          ret.allMarkdownRemark = ref.query.allMarkdownRemark;
+          ret.generator = "Claudia Valdivieso";
+          return ret;
         },
         query: `
         {
@@ -136,7 +136,7 @@ module.exports = {
         feeds: [
           {
             serialize(ctx) {
-              const { rssMetadata } = ctx.query.site.siteMetadata
+              const { rssMetadata } = ctx.query.site.siteMetadata;
               return ctx.query.allMarkdownRemark.edges.map(edge => ({
                 categories: edge.node.frontmatter.tags,
                 date: edge.node.fields.date,
@@ -145,10 +145,10 @@ module.exports = {
                 url: rssMetadata.site_url + edge.node.fields.slug,
                 guid: rssMetadata.site_url + edge.node.fields.slug,
                 custom_elements: [
-                  { 'content:encoded': edge.node.html },
-                  { author: config.userEmail },
-                ],
-              }))
+                  { "content:encoded": edge.node.html },
+                  { author: config.userEmail }
+                ]
+              }));
             },
             query: `
             {
@@ -178,10 +178,16 @@ module.exports = {
               }
             }
           `,
-            output: config.siteRss,
-          },
-        ],
-      },
+            output: config.siteRss
+          }
+        ]
+      }
     },
-  ],
-}
+    {
+      resolve: "gatsby-plugin-disqus",
+      options: {
+        shortname: "lavaldi"
+      }
+    }
+  ]
+};
