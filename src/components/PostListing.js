855bcf5299521d14efa235c514ed3e0dc64e7f74
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-10-03 19:34:14 -0500

    Filter posts server-side to fix posts count (#3)

--- src/components/PostListing.js
+++ src/components/PostListing.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import Img from 'gatsby-image'
-import moment from 'moment'
-import { formatDate } from '../utils/global'
+import React, { Component } from "react";
+import { Link } from "gatsby";
+import Img from "gatsby-image";
+import moment from "moment";
+import { formatDate } from "../utils/global";
 
 export default class PostListing extends Component {
   getPostList() {
-    const { postEdges } = this.props
-    const postList = postEdges
-      .filter(postEdge => postEdge.node.frontmatter.template === 'post')
-      .map(postEdge => {
-        return {
-          path: postEdge.node.fields.slug,
-          tags: postEdge.node.frontmatter.tags,
-          thumbnail: postEdge.node.frontmatter.thumbnail,
-          title: postEdge.node.frontmatter.title,
-          date: postEdge.node.fields.date,
-          excerpt: postEdge.node.excerpt,
-          timeToRead: postEdge.node.timeToRead,
-          categories: postEdge.node.frontmatter.categories,
-        }
-      })
-    return postList
+    const { postEdges } = this.props;
+    const postList = postEdges.map(postEdge => {
+      return {
+        path: postEdge.node.fields.slug,
+        tags: postEdge.node.frontmatter.tags,
+        thumbnail: postEdge.node.frontmatter.thumbnail,
+        title: postEdge.node.frontmatter.title,
+        date: postEdge.node.fields.date,
+        excerpt: postEdge.node.excerpt,
+        timeToRead: postEdge.node.timeToRead,
+        categories: postEdge.node.frontmatter.categories
+      };
+    });
+    return postList;
   }
 
   render() {
-    const { simple } = this.props
-    const postList = this.getPostList()
+    const { simple } = this.props;
+    const postList = this.getPostList();
 
     return (
-      <section className={`posts ${simple ? 'simple' : ''}`}>
+      <section className={`posts ${simple ? "simple" : ""}`}>
         {postList.map(post => {
-          let thumbnail
+          let thumbnail;
           if (post.thumbnail) {
-            thumbnail = post.thumbnail.childImageSharp.fixed
+            thumbnail = post.thumbnail.childImageSharp.fixed;
           }
 
-          const popular = post.categories.includes('Popular')
-          const date = formatDate(post.date)
-          const newest = moment(post.date) > moment().subtract(1, 'months')
+          const popular = post.categories.includes("Popular");
+          const date = formatDate(post.date);
+          const newest = moment(post.date) > moment().subtract(1, "months");
 
           return (
             <Link to={post.path} key={post.title}>
@@ -60,9 +58,9 @@ export default class PostListing extends Component {
                 )}
               </div>
             </Link>
-          )
+          );
         })}
       </section>
-    )
+    );
   }
 }
