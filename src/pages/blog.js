ee094c8899d40124f260236ef9197db781397ba6
Claudia Valdivieso <claumavaldivieso@gmail.com>
2019-10-03 19:31:35 -0500

    Filter posts server-side to fix posts count

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,63 +1,69 @@
-import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import { graphql } from 'gatsby'
-import Layout from '../layout'
-import PostListing from '../components/PostListing'
-import SEO from '../components/SEO'
-import config from '../../data/SiteConfig'
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../layout";
+import PostListing from "../components/PostListing";
+import SEO from "../components/SEO";
+import config from "../../data/SiteConfig";
 
 export default class BlogPage extends Component {
   state = {
-    searchTerm: '',
+    searchTerm: "",
     currentCategories: [],
     posts: this.props.data.posts.edges,
-    filteredPosts: this.props.data.posts.edges,
-  }
+    filteredPosts: this.props.data.posts.edges
+  };
 
   handleChange = async event => {
-    const { name, value } = event.target
+    const { name, value } = event.target;
 
-    await this.setState({ [name]: value })
+    await this.setState({ [name]: value });
 
-    this.filterPosts()
-  }
+    this.filterPosts();
+  };
 
   filterPosts = () => {
-    const { posts, searchTerm, currentCategories } = this.state
+    const { posts, searchTerm, currentCategories } = this.state;
 
     let filteredPosts = posts.filter(post =>
-      post.node.frontmatter.title.toLowerCase().includes(searchTerm.toLowerCase())
-    )
+      post.node.frontmatter.title
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    );
 
     if (currentCategories.length > 0) {
       filteredPosts = filteredPosts.filter(
         post =>
           post.node.frontmatter.categories &&
-          currentCategories.every(cat => post.node.frontmatter.categories.includes(cat))
-      )
+          currentCategories.every(cat =>
+            post.node.frontmatter.categories.includes(cat)
+          )
+      );
     }
 
-    this.setState({ filteredPosts })
-  }
+    this.setState({ filteredPosts });
+  };
 
   updateCategories = category => {
-    const { currentCategories } = this.state
+    const { currentCategories } = this.state;
 
     if (!currentCategories.includes(category)) {
       this.setState(prevState => ({
-        currentCategories: [...prevState.currentCategories, category],
-      }))
+        currentCategories: [...prevState.currentCategories, category]
+      }));
     } else {
       this.setState(prevState => ({
-        currentCategories: prevState.currentCategories.filter(cat => category !== cat),
-      }))
+        currentCategories: prevState.currentCategories.filter(
+          cat => category !== cat
+        )
+      }));
     }
-  }
+  };
 
   render() {
-    const { filteredPosts, searchTerm, currentCategories } = this.state
-    const filterCount = filteredPosts.length
-    const categories = this.props.data.categories.group
+    const { filteredPosts, searchTerm, currentCategories } = this.state;
+    const filterCount = filteredPosts.length;
+    const categories = this.props.data.categories.group;
 
     return (
       <Layout>
@@ -67,20 +73,20 @@ export default class BlogPage extends Component {
           <h1>Articles</h1>
           <div className="category-container">
             {categories.map(category => {
-              const active = currentCategories.includes(category.fieldValue)
+              const active = currentCategories.includes(category.fieldValue);
 
               return (
                 <div
-                  className={`category-filter ${active ? 'active' : ''}`}
+                  className={`category-filter ${active ? "active" : ""}`}
                   key={category.fieldValue}
                   onClick={async () => {
-                    await this.updateCategories(category.fieldValue)
-                    await this.filterPosts()
+                    await this.updateCategories(category.fieldValue);
+                    await this.filterPosts();
                   }}
                 >
                   {category.fieldValue}
                 </div>
-              )
+              );
             })}
           </div>
           <div className="search-container">
@@ -97,13 +103,17 @@ export default class BlogPage extends Component {
           <PostListing postEdges={filteredPosts} />
         </div>
       </Layout>
-    )
+    );
   }
 }
 
 export const pageQuery = graphql`
   query BlogQuery {
-    posts: allMarkdownRemark(limit: 2000, sort: { fields: [fields___date], order: DESC }) {
+    posts: allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [fields___date], order: DESC }
+      filter: { frontmatter: { template: { eq: "post" } } }
+    ) {
       edges {
         node {
           fields {
@@ -136,4 +146,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
