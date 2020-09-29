6067f563a6f18e167a09e81090c7e9182c4c0c1d
Claudia Valdivieso <claumavaldivieso@gmail.com>
2020-09-28 20:34:20 -0500

    Update siteUrl

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export default {
   siteTitle: 'Claudia Valdivieso',
-  siteUrl: 'https://www.lavaldi.com',
+  siteUrl: 'https://lavaldi.com',
   siteLogo: '/logo.png',
   description: 'Christian, wife, mother and front end.',
   userTwitter: 'lavaldi_',
