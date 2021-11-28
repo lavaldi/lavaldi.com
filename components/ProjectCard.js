f7b336de2d1c406e08bb13a4f218acbe6b0ef98a
Claudia Valdivieso <claumavaldivieso@gmail.com>
2021-11-27 22:11:12 -0500

    improve ProjectCard styles

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,16 +1,21 @@
-export default function ProjectCard({ title, description, href, icon }) {
+import ExternalLink from "./ExternalLink";
+
+export default function ProjectCard({
+  title,
+  description,
+  githubUrl,
+  webUrl,
+  icon
+}) {
   return (
-    <a
-      className="w-full mb-4 hover:shadow"
-      href={href}
+    <div
+      className="w-full mb-4 hover:shadow rounded bg-gradient-to-r from-purple-300 via-red-400 to-purple-600 p-0.5"
       aria-label={title}
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="flex items-center border border-purple-500 dark:border-gray-800 rounded p-4">
-        <div className="h-8 w-8 ml-2 mr-4 text-2xl">
-          {icon}
-        </div>
+      <div className="flex flex-wrap items-center bg-purple-50 dark:bg-black rounded p-4">
+        <div className="h-8 w-8 ml-2 mr-4 text-2xl">{icon}</div>
         <div>
           <h4 className="text-lg font-bold tracking-tight text-purple-900 dark:text-gray-100">
             {title}
@@ -19,7 +24,26 @@ export default function ProjectCard({ title, description, href, icon }) {
             {description}
           </p>
         </div>
+        <div className="ml-auto flex justify-between w-full mt-2 sm:block sm:w-auto sm:mt-0">
+          {githubUrl ? (
+            <p className="leading-5 text-gray-700 dark:text-gray-300 text-right">
+              <ExternalLink
+                className="underline sm:no-underline"
+                href={githubUrl}
+              >
+                GitHub
+              </ExternalLink>
+            </p>
+          ) : null}
+          {webUrl ? (
+            <p className="leading-5 text-gray-700 dark:text-gray-300 text-right">
+              <ExternalLink className="underline sm:no-underline" href={webUrl}>
+                Web
+              </ExternalLink>
+            </p>
+          ) : null}
+        </div>
       </div>
-    </a>
+    </div>
   );
 }
