df2f39d654c5c5ff6e8fb6dad356c3ac951e5eb4
Claudia Valdivieso <claumavaldivieso@gmail.com>
2022-02-09 21:54:01 -0500

    remove unnecessary classes

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,13 @@ import ExternalLink from '@/components/ExternalLink';
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+    <footer className="w-full mb-8">
       <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
-      <div className="w-full max-w-2xl grid grid-cols-2 gap-1 pb-16 sm:grid-cols-4">
+      <div className="w-full max-w-2xl mx-auto grid grid-cols-2 gap-1 pb-16 sm:grid-cols-4">
         <Link href="/">
-          <a className="text-purple-800 hover:text-purple-900 dark:text-gray-500 dark:hover:text-gray-600 transition">Home</a>
+          <a className="text-purple-800 hover:text-purple-900 dark:text-gray-500 dark:hover:text-gray-600 transition">
+            Home
+          </a>
         </Link>
         <Link href="/about">
           <a className="text-purple-800 hover:text-purple-900 dark:text-gray-500 dark:hover:text-gray-600 transition">
@@ -23,7 +25,9 @@ export default function Footer() {
         <ExternalLink
           className="text-purple-800 hover:text-purple-900 dark:text-gray-500 dark:hover:text-gray-600 transition"
           href="https://github.com/lavaldi"
-        >GitHub</ExternalLink>
+        >
+          GitHub
+        </ExternalLink>
         {/* <Link href="/newsletter">
             <a className="text-gray-500 hover:text-gray-600 transition">
               Newsletter
